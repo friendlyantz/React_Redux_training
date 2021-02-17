c328db2aa80086adbe9557348992d48e8baed0e9
Anton <panteleev23@gmail.com>
2021-02-17 18:54:37 +1100

    ex complete

--- 01-React/01-Tooling/02-Manipulating-Strings/lib/02_counting_words.js
+++ 01-React/01-Tooling/02-Manipulating-Strings/lib/02_counting_words.js
@@ -1,5 +1,20 @@
 function occurrences(text, word) {
-  return null;
+  // return null;
+  // 1. breakworn text ito words
+  // 2. save into array
+  // loop comparator with 'keyword'
+  // counter for occurance +=
+  console.log("....starting...");
+  let wordsArray = 0;
+  wordsArray = text.split(' ');
+  console.log(wordsArray);
+  let counter = 0;
+  for (const i of wordsArray) {
+    if (i.toLowerCase() === word.toLowerCase()) {
+      counter += 1;
+    }
+  }
+  return counter;
 }
 
 module.exports = occurrences;
